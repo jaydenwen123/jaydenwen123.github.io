e3ab5bde44751261291a8111321ec455833fb166
jaydenwen123 <2282186474@qq.com>
2021-04-17 14:07:50 +0800

    feat:update book content

--- boltdb/deploy.sh
+++ boltdb/deploy.sh
@@ -4,6 +4,16 @@ rm -rf  _book/
 echo "[INFO] execute gitbook build to blog directory!"
 gitbook build .  ../jaydenwen123.github.io/boltdb
 
+rm -rf _book/
+
+git add .
+
+git commit -m "feat:update book content"
+
+git push origin master
+
+echo "[INFO] execute deploy the book to website"
+
 cd ../jaydenwen123.github.io
 
 git add .
